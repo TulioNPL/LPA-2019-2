872708b8165a5ba606d84b467750fa95eea6c145
TulioNPL <tulionp.lopes@gmail.com>
2019-08-07 19:57:43 -0300

    Primeiro Commit

---
+++ 01/LPA01.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int dimension;
+    cin >> dimension;
+    int matrix[dimension][dimension];
+
+    for(int i = 0; i < dimension; i++) {
+        for (int j = 0; j < dimension; j++){
+            cin >> matrix[i][j];
+        }
+    }
+    
+    for(int i = 0; i < dimension; i++) {
+        for (int j = 0; j < dimension; j++){
+            cout << matrix[i][j]; 
+            cout << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
