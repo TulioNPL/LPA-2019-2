ac726f15b04589db497d27ca4ee9c9c5c8e01d49
TulioNPL <tulionp.lopes@gmail.com>
2019-08-13 12:15:12 -0300

    Lendo as entradas

--- 02/LPA02.cpp
+++ 02/LPA02.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <sstream>
+#include <vector>
 using namespace std;
 
 int main() {
+    vector<double> input;
+    string str;
+  
+    do{
+        double entrada;
+       
+        ostringstream strs;
+
+        cin >> entrada;
+        input.push_back(entrada);
+
+        strs << entrada;
+        str = strs.str();
+    }while(str.compare("0") != 0);
+
+    for(auto i = input.begin(); i != input.end(); i++) {
+        printf("%.2f\n", *i);
+    }
+
     return 0;
 }
