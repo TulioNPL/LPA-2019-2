2fac9015b7476b39510c1a8e6d5c11031889ded4
Túlio Polido <tuliopolido@MacBook-Pro-de-Tulio.local>
2019-08-28 12:39:04 -0300

    Questão Finalizada

--- 04/LPA04.cpp
+++ 04/LPA04.cpp
@@ -1,14 +1,31 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
+/*
+Pega o substring para retirar os espacos em branco no inicio e no fim
+*/
 string trim(string& str) {
     size_t first = str.find_first_not_of(' ');
     size_t last = str.find_last_not_of(' ');
     return str.substr(first, (last-first+1));
 }
 
+/*
+Remove os espacos em branco no meio da String
+*/
 string rmWhiteSpaces(string& str) {
     string nova = "";
+
+    for(int i = 0; i < str.size(); i++){
+        if(str[i] == ' '){
+            nova += str[i];
+            while(str[i] == ' ') i++;
+            nova += str[i];
+        } else {
+            nova += str[i];
+        }
+    }
 
     return nova;
 }
@@ -27,12 +44,11 @@ int main(){
             getline(cin,linhas[i]);
             linhas[i] = trim(linhas[i]);
             linhas[i] = rmWhiteSpaces(linhas[i]);
-            if(linhas[i].size() > maiorString) maiorString = linhas[i].size();
+            if(linhas[i].size() > maiorString) maiorString = linhas[i].size();  //salva o tamanho da maiorString
         }
-        cout << maiorString << endl;
         
         for(int i = 0; i < N; i++){
-            cout << linhas[i] << endl;
+            cout << right << setw(maiorString) << linhas[i] << endl;
         }
         cout << endl;
         cin >> N;
