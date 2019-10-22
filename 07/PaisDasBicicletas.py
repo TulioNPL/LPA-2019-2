dde8aa0380628e1a01962b9b37983cee191125d9
Túlio Polido <tuliopolido@MacBook-Pro-de-Tulio.local>
2019-10-22 06:23:43 -0300

    Resposta incorreta

--- 07/PaisDasBicicletas.py
+++ 07/PaisDasBicicletas.py
@@ -10,16 +10,10 @@ class Graph():
     def printSolution(self, dist,sink): 
         print(dist[sink])
   
-    # A utility function to find the vertex with  
-    # minimum distance value, from the set of vertices  
-    # not yet included in shortest path tree 
     def minDistance(self, dist, sptSet): 
   
-        # Initilaize minimum distance for next node 
         min = sys.maxsize 
   
-        # Search not nearest vertex not in the  
-        # shortest path tree 
         for v in range(self.V): 
             if dist[v] < min and sptSet[v] == False: 
                 min = dist[v] 
@@ -27,9 +21,6 @@ class Graph():
   
         return min_index 
   
-    # Funtion that implements Dijkstra's single source  
-    # shortest path algorithm for a graph represented  
-    # using adjacency matrix representation 
     def dijkstra(self, src, sink): 
   
         dist = [sys.maxsize] * self.V 
@@ -38,24 +29,16 @@ class Graph():
   
         for cout in range(self.V): 
   
-            # Pick the minimum distance vertex from  
-            # the set of vertices not yet processed.  
-            # u is always equal to src in first iteration 
             u = self.minDistance(dist, sptSet) 
   
-            # Put the minimum distance vertex in the  
-            # shotest path tree 
             sptSet[u] = True
   
-            # Update dist value of the adjacent vertices  
-            # of the picked vertex only if the current  
-            # distance is greater than new distance and 
-            # the vertex in not in the shotest path tree 
             for v in range(self.V): 
                 if self.graph[u][v] > 0 and sptSet[v] == False and dist[v] > dist[u] + self.graph[u][v]: 
                         dist[v] = dist[u] + self.graph[u][v] 
   
         self.printSolution(dist,sink)
+        print(dist)
 
 #Driver
 n, m = map(int, input().split())
