999e090107dad00b47f65fe07ed80e5bd4a2149a
Túlio Polido <tuliopolido@MacBook-Pro-de-Tulio.local>
2019-10-22 05:58:53 -0300

    Erro de identacao

--- 07/PaisDasBicicletas.py
+++ 07/PaisDasBicicletas.py
@@ -1,17 +1,52 @@
-n, m = map(int, input().split())
-while n+m != 0:
-	grafo = [] #LISTA DE ARESTAS
+import sys 
+  
+class Graph(): 
+    def __init__(self, vertices): 
+        self.V = vertices 
+        self.graph = [[0 for column in range(vertices)]  
+                      for row in range(vertices)] 
+  
+    def minDistance(self, dist, sptSet): 
+        min = sys.maxsize 
+  
+        for v in range(self.V): 
+            if dist[v] < min and sptSet[v] == False: 
+                min = dist[v] 
+                min_index = v 
+  
+        return min_index 
+  
+    def dijkstra(self, src, sink): 
+        dist = [sys.maxsize] * self.V 
+        dist[src] = 0
+        sptSet = [False] * self.V 
+  
+        for cout in range(self.V): 
+            u = self.minDistance(dist, sptSet) 
+            sptSet[u] = True
+  
+            for v in range(self.V): 
+                if self.graph[u][v] > 0 and sptSet[v] == False and dist[v] > dist[u] + self.graph[u][v]: 
+                        dist[v] = dist[u] + self.graph[u][v]
 
+	print(dist[sink])
+
+#Driver
+n, m = map(int, input().split())
+count = 1
+while n+m != 0:
+	g = Graph(n)
 	for iterator in range(0,m):
 		i, j, h = map(int, input().split())
-		grafo.append((i,j,h))
-		KruskalMST(grafo,n)
-	k = int(input())
+		g.graph[i-1][j-1] = h
+		g.graph[j-1][i-1] = h
 
+	print("Instancia " + str(count),end='\n')
+
+	k = int(input())
 	for iterator in range(0,k):
 		i, j = map(int, input().split())
-		#Pegar menor caminho entre cada par de vertices i e j
+		g.dijkstra(i-1,j-1)
 
 	n, m = map(int, input().split())
-
-
+	count+=1
