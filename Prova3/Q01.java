e1790a4c0f3343b37a82b54b243622d86a05510f
Túlio Nunes Polido Lopes <PUCMINAS\947277@pmg34infll20504.pucminas.net>
2019-11-26 11:56:46 -0300

    Questão 01 finalizada

--- Prova3/Q01.java
+++ Prova3/Q01.java
@@ -1,5 +1,46 @@
-public class Q01 {
-	public static void main(String[] args){
-		System.out.println("Teste");
+import java.util.Scanner;
+import java.io.*;
+
+class Q01 {
+	public static void main(String[] args) throws Exception{
+		Scanner in = new Scanner(System.in);
+		
+		String linha;
+		
+		while (in.hasNextLine()) {
+			linha = in.nextLine();
+			int n = Integer.parseInt(linha.trim());
+			linha = in.nextLine();
+			int custoDia = Integer.parseInt(linha.trim());
+			
+			int[] lucros = new int[n];
+			for(int i = 0; i < n; i++) {
+				linha = in.nextLine();
+				lucros[i] = Integer.parseInt(linha.trim()) - custoDia;
+			}
+			
+			kadane(lucros,n);
+		}
+	}
+	
+	public static void kadane(int [] lucros, int n) {
+		int maxAtual = 0;
+		int maxTotal = -1;
+		int temp = 0;
+		
+		for(int i = 0; i < n; i++) {
+			maxAtual += lucros[i];
+			
+			if(maxAtual < 0) {
+				maxAtual = 0;
+				temp = i+1;
+			}
+			
+			if(maxAtual > maxTotal) {
+				maxTotal = maxAtual;
+			}
+		}
+		
+		System.out.println(maxTotal);
 	}
 }
